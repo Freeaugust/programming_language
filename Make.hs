ef7e4585f744bf11015d257ff068855962c29345
Freeaugust <zlin17@ucsc.edu>
2016-03-04 22:55:58 -0800

    zheng_lin_3_4

--- Make.hs
+++ Make.hs
@@ -1,7 +1,16 @@
 import Moduling
 import Show
 
-m = make_Html "head"
+url = make_Url "http://www.ucsc.edu"
 
-main = let tmp = eval_r m in
-       putStr tmp
+l_1 = make_Level 1
+l_2 = make_Level 2
+l_3 = make_Level 3
+l_4 = make_Level 4
+l_5 = make_Level 5
+l_6 = make_Level 6
+
+root = make_Html "Short Test Web"
+title = make_Basc "third test"
+
+main = print "fsa"

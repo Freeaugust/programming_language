d4f1275efad13e61134e6bdeb5d19dd116e680fa
Freeaugust <zlin17@ucsc.edu>
2016-03-04 22:55:44 -0800

    zheng_lin_3_4

--- Moduling.hs
+++ Moduling.hs
@@ -1,16 +1,19 @@
 
 module Moduling (
-	Url(Url),
 	Level(Level),
-	Inp(Inp),
+	In_att(Url, Inp, Detail),
 	Attribute(Empty_a, Alt, Disable, Href, Id, Src, Style, Cmb_a),
-	Elem(A, P, Div, Empty_e, Img, H, Cmb_e, Link),
+	Elem(A, P, Div, Empty_e, Img, H, Cmb_e),
 	Basc(Title, Body, Empty_b, Cmb_b),
 	Head(Head),
 	Root(Html),
+	make_Att,
 	make_Html,
 	make_Basc,
 	make_Elem,
+	make_Url,
+	make_Inp,
+	make_Detail,
 	make_Level_h,
 	make_Level,
 	cmb_Elem,
@@ -19,16 +22,18 @@ module Moduling (
 	add_Basc
   )where
 
-data Url = Url String deriving(Show, Read, Eq)
 data Level = Level Int deriving(Show, Read, Eq)
-data Inp = Inp String deriving(Show, Read, Eq)
+data In_att = Url String
+			| Inp String
+			| Detail String
+			deriving(Show, Read, Eq)
 data Attribute = Empty_a
-			   | Alt String
-			   | Disable Inp
-			   | Href Url
-			   | Id String
-			   | Src Url
-			   | Style String
+			   | Alt In_att
+			   | Disable In_att
+			   | Href In_att
+			   | Id In_att
+			   | Src In_att
+			   | Style In_att
 			   | Cmb_a Attribute Attribute 
 			   deriving(Show, Read, Eq)
 data Elem = A Attribute String
@@ -38,7 +43,6 @@ data Elem = A Attribute String
 		  | Img Attribute
 		  | H Level String
 		  | Cmb_e Elem Elem
-		  | Link Url
 		  deriving(Show, Read, Eq)
 data Basc = Title String
 		  | Body String Elem
@@ -47,6 +51,15 @@ data Basc = Title String
 		  deriving(Show, Read, Eq)
 data Head = Head String deriving(Show, Read, Eq)
 data Root = Html Head Basc deriving(Show, Read, Eq)
+
+make_Att :: String->In_att->Attribute
+make_Att type_name s = if type_name=="alt" then Alt s
+					   else if type_name=="disable" then Disable s
+					   else if type_name=="href" then Href s
+					   else if type_name=="id" then Id s
+					   else if type_name=="src" then Src s
+					   else if type_name=="style" then Style s
+					   else Empty_a
 
 make_Html :: String->Root
 make_Html name = Html (Head name) Empty_b
@@ -61,6 +74,15 @@ make_Elem type_name att content = if type_name=="a" then A att content
 								  else if type_name=="p" then P content
 								  else if type_name=="img" then Img att
 								  else Empty_e
+
+make_Url :: String->In_att
+make_Url http = Url http
+
+make_Inp :: String->In_att
+make_Inp s = Inp s
+
+make_Detail :: String->In_att
+make_Detail detail = Detail detail
 
 make_Level_h :: Level->String->Elem
 make_Level_h level title = H level title
